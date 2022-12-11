e1b3fe4c8c19b63a51bc08c27aebdc939f2c2e8e
Tomas Karabela <tkarabela@seznam.cz>
2022-12-11 01:40:26 +0100

    OfxDescribe flatbuffers mockup

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include "OpenMfx/Sdk/Cpp/Host/Host"
 #include "OpenMfx/Sdk/Cpp/Host/EffectRegistry"
 #include "OpenMfx/Sdk/Cpp/Host/EffectLibrary"
 #include "OpenMfx/Sdk/Cpp/Host/MeshEffect"
 #include "OpenMfx/Sdk/Cpp/Host/MeshProps"
 #include "OpenMfx/Sdk/Cpp/Host/Mesh"
+#include "messages_generated.h"
 
 int main() {
     OpenMfx::Host host;
     OpenMfx::EffectRegistry& registry = OpenMfx::EffectRegistry::GetInstance();
     registry.setHost(&host);
-    auto library = registry.getLibrary("./OpenMfx/sdk/examples/c/plugins/OpenMfx_Example_C_Plugin_color_to_uv.ofx");
+    auto library = registry.getLibrary("./cmake-build-debug/OpenMfx/sdk/examples/c/plugins/OpenMfx_Example_C_Plugin_color_to_uv.ofx");
 
     std::cout << "Hello, I see " << library->effectCount() << " effects" << std::endl;
     std::cout << "First effect is " << library->effectIdentifier(0) << std::endl;
@@ -55,7 +57,60 @@ int main() {
         }
     }
 
-    OfxMeshEffectHandle effectInstance;
+  // flatbuffers
+  flatbuffers::FlatBufferBuilder builder(1024);
+    auto plugin_name = builder.CreateString(library->effectIdentifier(0));
+  std::vector<flatbuffers::Offset<OpenMfxRemoteProxy::OfxMesh>> inputs;
+
+  for (auto& effectInput : effectDescriptor->inputs) {
+    std::vector<flatbuffers::Offset<OpenMfxRemoteProxy::OfxProperty>> input_properties;
+
+    for (auto& prop : effectInput.properties) {
+      auto property_name = builder.CreateString(prop.index());
+
+      if (prop.type == OpenMfx::PropertyType::Int) {
+        auto values = builder.CreateVector(&(prop.value[0].as_int), 1); // TODO determine size
+        auto property_value = OpenMfxRemoteProxy::CreateOfxPropertyIntNValue(builder, values);
+        input_properties.push_back(OpenMfxRemoteProxy::CreateOfxProperty(builder, property_name,
+                                                                         OpenMfxRemoteProxy::OfxPropertyValue_OfxPropertyIntNValue,
+                                                                         property_value.Union()));
+      } else if (prop.type == OpenMfx::PropertyType::String) {
+        auto values = builder.CreateVectorOfStrings(&(prop.value[0].as_const_char), &(prop.value[0].as_const_char)+1); // TODO determine size
+        auto property_value = OpenMfxRemoteProxy::CreateOfxPropertyStringNValue(builder, values);
+        input_properties.push_back(OpenMfxRemoteProxy::CreateOfxProperty(builder, property_name,
+                                                                         OpenMfxRemoteProxy::OfxPropertyValue_OfxPropertyStringNValue,
+                                                                         property_value.Union()));
+      } else {
+        std::cout <<"XXX not writing to flatbuffer a property" <<std::endl;
+      }
+    }
+
+    auto properties_vec = builder.CreateVector(input_properties);
+    auto mesh = OpenMfxRemoteProxy::CreateOfxMesh(builder, properties_vec);
+    inputs.push_back(mesh);
+  }
+
+  auto inputs_vec = builder.CreateVector(inputs);
+  auto describe = OpenMfxRemoteProxy::CreateOpenMfxRemoteProxyDescribe(builder, plugin_name, inputs_vec);
+
+  std::cout <<"finishing flatbuffer builder" <<std::endl;
+  builder.Finish(describe);
+  // This must be called after `Finish()`.
+  uint8_t *buf = builder.GetBufferPointer();
+  auto bufsize = builder.GetSize(); // Returns the size of the buffer that
+  // `GetBufferPointer()` points to.
+  std::cout <<"writing flatbuffer to file" <<std::endl;
+  std::ofstream outfile;
+  outfile.open("temp/describe_test.bin", std::ios::binary | std::ios::out);
+  if (!outfile.good()) {
+    std::cout <<"bad outfile!" <<std::endl;
+
+  }
+
+  outfile.write(reinterpret_cast<const char *>(buf), bufsize);
+  outfile.close();
+
+  OfxMeshEffectHandle effectInstance;
     bool ok = host.CreateInstance(effectDescriptor, effectInstance);
     std::cout << "Created instance? " << ok <<std::endl;
 
